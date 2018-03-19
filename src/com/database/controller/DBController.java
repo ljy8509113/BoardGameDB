08004c3ce3c2e16b5bd412f9d7897581caaedba0
ljy850113 <ljy8509113@nate.com>
2018-03-19 09:57:23 +0900

    controller 함수 추가 영역 구분

--- src/com/database/controller/DBController.java
+++ src/com/database/controller/DBController.java
@@ -16,27 +16,40 @@ import com.database.util.CustomException;
 
 public class DBController {
 	private static DBController instance = null;
-	
+
 	public static DBController Instance() {
 		if(instance == null) {
 			instance = new DBController();
 			instance.userDao = new UserDao(); 
 		}
-		
+
 		return instance;
 	}
-	
+
 	UserDao userDao = null;
-	
+
 	public User login(String email, String password) throws InvalidKeyException, ClassNotFoundException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, CustomException, SQLException{
 		return userDao.selectUser(email, password);
 	}
-	
+
 	public User selectUser(String email) throws ClassNotFoundException, SQLException, CustomException {
 		return userDao.selectUser(email);
 	}
-	
+
 	public void join(User user) throws InvalidKeyException, ClassNotFoundException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, CustomException, SQLException {
 		userDao.insert(user);
 	}
+
+
+
+
+	//---여정--------------------------------------//
+
+
+
+	//---성은--------------------------------------//
+
+
+
+	//---정욱--------------------------------------//
 }
